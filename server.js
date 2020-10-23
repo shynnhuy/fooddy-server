ee7537d6d0c1036b2f2799477a2dd8b6f752eb18
shynnhuy <shynn@hotmail.com>
2020-10-23 13:46:53 +0700

    fix

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var express = require('express');
+// var express = require('express');
 const jsonServer = require("json-server");
-// const server = jsonServer.create();
+const server = jsonServer.create();
 var server = express();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-server.use(express.json())
+// server.use(express.json())
 server.use(middlewares);
 server.use(router);
 
-server.listen(80, () => {
+server.listen(3000, () => {
   console.log("JSON Server is running");
 });
